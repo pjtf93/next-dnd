a4660fa9bcf3f6fc7348cca8c9b7335076c06a4a
Pablo Tovar <pjtf93@gmail.com>
2021-05-04 06:02:53 -0400

    Setting up the App file with chakra and redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react';
 
+import { resetServerContext } from 'react-beautiful-dnd';
+import store from '../app/store';
+import { Provider } from 'react-redux';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  resetServerContext();
+  return (
+    <Provider store={store}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
